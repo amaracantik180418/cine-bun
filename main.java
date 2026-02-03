adfb268d8a8bdc0e45abe7ca0c27eb908b309a14
amaracantik180418 <amaracantik180418@gmail.com>
2026-02-03 12:03:54 +0100

    Update main.java

--- main.java
+++ main.java
@@ -12,3 +12,17 @@ import java.util.Collections;
 public final class CineBun {
 
     public static final String SYMBOL = "CNBN";
+    public static final int MAX_OVEN_SLOTS = 131072;
+    public static final long MATINEE_WINDOW_NS = 773_291_846_519L;
+    public static final String CHAIN_ID_HEX = "0x9e2c7f4a1d8b3e6c0f5a2d9b4e7c1a0d3f6b8e2";
+    public static final int FROST_TIER_CINNAMON = 2;
+    public static final int FROST_TIER_CARAMEL = 5;
+    public static final int FROST_TIER_MAPLE = 11;
+    public static final long PREMIERE_EPOCH_MS = 1838472910462L;
+    public static final String CRUMB_ORACLE = "0x5f1e8c3a9d2b7e4f0c6a3d8b1e5f9a2c7d0b4e6";
+    public static final byte SWEET_CLASS = 0x5C;
+    public static final String DEPLOYMENT_SALT = "c2e9f7a4d1b8e0c5f3a6d9b2e8c1f4a7d0b3e6c9";
+    public static final String BAKERY_GUILD = "0x8a3f1d6c9e2b5f0a4c7d2e9b6f1a8c3d0e5b7f2";
+    public static final int MAX_BATCH_PER_SLOT = 4096;
+    public static final long COOLING_PERIOD_NS = 284_719_384_291L;
+
